752dc7df57e4a6af105c12703ed8203a4aadbc67
iso2013 <ejwmercer@gmail.com>
2018-04-24 14:05:28 -0600

    Javadocs part 2

--- API/src/main/java/net/blitzcube/peapi/api/entity/hitbox/IHitbox.java
+++ API/src/main/java/net/blitzcube/peapi/api/entity/hitbox/IHitbox.java
@@ -3,18 +3,48 @@ package net.blitzcube.peapi.api.entity.hitbox;
 import org.bukkit.util.Vector;
 
 /**
- * Created by iso2013 on 4/21/2018.
+ * @author iso2013
+ * @version 0.1
+ * @since 2018-04-23
  */
 public interface IHitbox {
+    /**
+     * Gets the minimum corner of this hitbox.
+     *
+     * @return the {@link Vector} representing the minimum corner of this hitbox
+     */
     Vector getMin();
 
+    /**
+     * Sets the minimum corner of this hitbox.
+     * @param vector the {@link Vector} that should be used as the minimum corner of this hitbox
+     */
     void setMin(Vector vector);
 
+    /**
+     * Gets the maximum corner of this hitbox.
+     * @return the {@link Vector} representing the maximum corner of this hitbox
+     */
     Vector getMax();
 
+    /**
+     * Sets the maximum corner of this hitbox.
+     * @param vector the {@link Vector} that should be used as the maximum corner of this hitbox
+     */
     void setMax(Vector vector);
 
+    /**
+     * Checks whether or not a given line of sight is intersecting the hitbox. Uses an algorithm adapted from
+     * @param origin the location that the line of sight starts at
+     * @param angle the direction the line of sight is facing
+     * @param hitboxLocation the location that the hitbox's origin is at
+     * @return whether or not the line of sight intersects
+     */
     boolean intersects(Vector origin, Vector angle, Vector hitboxLocation);
 
+    /**
+     * Creates a copy of this hitbox, cloning the stored vectors as well.
+     * @return a new hitbox instance.
+     */
     IHitbox deepClone();
 }
--- API/src/main/java/net/blitzcube/peapi/api/entity/modifier/IEntityIdentifier.java
+++ API/src/main/java/net/blitzcube/peapi/api/entity/modifier/IEntityIdentifier.java
@@ -8,21 +8,64 @@ import java.lang.ref.WeakReference;
 import java.util.UUID;
 
 /**
- * Created by iso2013 on 2/23/2018.
+ * @author iso2013
+ * @version 0.1
+ * @since 2018-02-23
  */
 public interface IEntityIdentifier {
-
+    /**
+     * Attempts to find more information about the entity that this identifies.
+     * For example, if the identifier was created by a packet, it will contain the entity ID but not the entity or fake
+     * entity object. Calling this method will lookup entities with a matching ID and update the references.
+     */
     void moreSpecific();
 
+    /**
+     * Gets the entity ID that is associated with this identifier. This is never undefined.
+     *
+     * @return the entity ID associated with this identifier
+     */
     int getEntityID();
 
+    /**
+     * Gets the {@link UUID} that is associated with this identifier. This may be null if the identifier was constructed by a
+     * packet listener, in which case we have no information about the entity that the ID belongs to. We can attempt to
+     * find this if it is not present by using {@link #moreSpecific()}
+     * @return the {@link UUID} of the entity that this is associated with
+     */
     UUID getUUID();
 
+    /**
+     * Gets the reference to the player that this identifier was constructed for. This will be null unless the
+     * identifier was retrieved from a packet event, in which case it will be the player that the event is being fired
+     * for.
+     * @return the {@link Player} that this identifier was created for
+     */
     WeakReference<Player> getNear();
 
+    /**
+     * Gets the reference to the entity that this identifier identifies. This may be null if the identifier was constructed by a
+     * packet listener, in which case we have no information about the entity that the ID belongs to. We can attempt to
+     * find this if it is not present by using {@link #moreSpecific()}
+     * <br>
+     * If this is a fake entity, this value will be null.
+     * @return the entity that this identifier is referencing
+     */
     WeakReference<Entity> getEntity();
 
+    /**
+     * Gets the reference to the fake entity that this identifier identifies. This may be null if the identifier was
+     * constructed by a packet listener, in which case we have no information about the entity that the ID belongs to.
+     * We can attempt to find this if it is not present by using {@link #moreSpecific()}
+     * <br>
+     * If this is a real entity, this value will be null.
+     * @return the fake entity that this identifier is referencing.
+     */
     WeakReference<IFakeEntity> getFakeEntity();
 
+    /**
+     * Returns whether or not this entity is a fake entity.
+     * @return whether or not this is a fake entity
+     */
     boolean isFakeEntity();
 }

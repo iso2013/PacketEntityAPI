dc11eec9b18862627225eed510705b30397bdbc7
iso2013 <blitzcubeowner@gmail.com>
2018-05-24 11:44:33 -0600

    Remove unnecessary and complex shading system.

--- Plugin/src/main/java/net/blitzcube/peapi/PacketEntityAPI.java
+++ Plugin/src/main/java/net/blitzcube/peapi/PacketEntityAPI.java
@@ -23,22 +23,17 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.RegisteredServiceProvider;
-import org.bukkit.plugin.ServicePriority;
-import org.bukkit.plugin.ServicesManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
-import java.util.function.Consumer;
 
 /**
  * Created by iso2013 on 2/13/2018.
  */
-public class PacketEntityAPI implements IPacketEntityAPI {
+public class PacketEntityAPI extends JavaPlugin implements IPacketEntityAPI {
     public static final Map<EntityType, Integer> OBJECTS = new HashMap<>();
 
     static {
@@ -73,81 +68,39 @@ public class PacketEntityAPI implements IPacketEntityAPI {
         OBJECTS.put(EntityType.LIGHTNING, -1);
     }
 
-    private static boolean compareVersions(String fullVersion) {
-        String[][] input = new String[][]{fullVersion.split("\\."), ProviderStub.FULL_VERSION.split("\\.")};
-        int size = Math.max(input[0].length, input[1].length);
-        for (int s = 0; s < size; s++) {
-            if (input[0].length - 1 < s) return true;
-            if (input[1].length - 1 < s) return false;
-            if (input[0][s].equals(input[1][s])) continue;
-            return Integer.valueOf(input[0][s]) < Integer.valueOf(input[1][s]);
-        }
-        return false;
-    }
-
-    private static JavaPlugin parent;
     private static IPacketEntityAPI instance;
 
     private static TaskChainFactory chainFactory;
     /*
      * Begin actual API implementation:
      */
-    private final EntityModifierRegistry modifierRegistry;
-    private final ProtocolManager manager;
+    private EntityModifierRegistry modifierRegistry;
+    private ProtocolManager manager;
     private FakeEntityFactory fakeEntityFactory;
     private EntityPacketFactory packetFactory;
     private PacketEventDispatcher dispatcher;
 
-    private PacketEntityAPI() {
+    @Override
+    public void onEnable() {
         this.modifierRegistry = new EntityModifierRegistry();
         this.manager = ProtocolLibrary.getProtocolManager();
         this.fakeEntityFactory = new FakeEntityFactory(this);
         this.packetFactory = new EntityPacketFactory();
         this.dispatcher = new PacketEventDispatcher(this, manager);
 
-        chainFactory = BukkitTaskChainFactory.create(parent);
+        chainFactory = BukkitTaskChainFactory.create(this);
+
+        if (instance == null) instance = this;
+
     }
 
-    public static void initialize(JavaPlugin parent, Consumer<IPacketEntityAPI> onLoad) {
-        ServicesManager m = Bukkit.getServicesManager();
+    @Override
+    public void onDisable() {
 
-        Collection<RegisteredServiceProvider<IPacketEntityAPI.ProviderStub>> r = m.getRegistrations(IPacketEntityAPI
-                .ProviderStub.class);
-        Optional<RegisteredServiceProvider<IPacketEntityAPI.ProviderStub>> top = r.stream()
-                .filter(p -> ProviderStub.MAJOR_VERSION.equals(p.getProvider().getMajorVersion())).findFirst();
-        if (top.isPresent()) {
-            if (compareVersions(top.get().getProvider().getFullVersion())) {
-                m.unregister(IPacketEntityAPI.ProviderStub.class, top.get());
-            } else return;
-        }
-        m.register(IPacketEntityAPI.ProviderStub.class, new PacketEntityAPI.ProviderStub(), parent, ServicePriority
-                .Normal);
-
-        if (PacketEntityAPI.parent == null || parent.getName().equals("PacketEntityAPI")) {
-            PacketEntityAPI.parent = parent;
-        }
-
-        PacketEntityAPI.instance = getLatestCompatibleVersion();
-
-        if (onLoad != null)
-            Bukkit.getScheduler().runTask(parent, () -> onLoad.accept(instance));
     }
 
     public static TaskChainFactory getChainFactory() {
         return chainFactory;
-    }
-
-    private static IPacketEntityAPI getLatestCompatibleVersion() {
-        RegisteredServiceProvider<IPacketEntityAPI.ProviderStub> match = Bukkit.getServicesManager()
-                .getRegistrations(IPacketEntityAPI.ProviderStub.class).stream()
-                .filter(p -> ProviderStub.MAJOR_VERSION.equals("0") &&
-                        ProviderStub.FULL_VERSION.equals(p.getProvider().getFullVersion()) ||
-                        ProviderStub.MAJOR_VERSION.equals(p.getProvider().getMajorVersion())).findFirst()
-                .orElse(null);
-        if (match == null) {
-            throw new IllegalStateException("No compatible API version found! List of current versions:");
-        }
-        return match.getProvider().getInstance();
     }
 
     public static IFakeEntity getFakeEntity(int entityID) {
@@ -221,7 +174,7 @@ public class PacketEntityAPI implements IPacketEntityAPI {
         PacketContainer c = packet.getRawPacket();
         if (c == null) return;
         if (delay > 0) {
-            Bukkit.getScheduler().scheduleSyncDelayedTask(parent, () -> {
+            Bukkit.getScheduler().scheduleSyncDelayedTask(this, () -> {
                 if (c.getType().isClient()) {
                     safeReceive(target, c);
                 } else {
@@ -255,26 +208,5 @@ public class PacketEntityAPI implements IPacketEntityAPI {
 
     public Collection<FakeEntity> getFakeEntities() {
         return fakeEntityFactory.getFakeEntities();
-    }
-
-    private static class ProviderStub implements IPacketEntityAPI.ProviderStub {
-        static final String FULL_VERSION = "${project.version}";
-        static final String MAJOR_VERSION = FULL_VERSION.substring(0, FULL_VERSION.indexOf("."));
-        private static PacketEntityAPI inst;
-
-        @Override
-        public String getMajorVersion() {
-            return MAJOR_VERSION;
-        }
-
-        @Override
-        public String getFullVersion() {
-            return FULL_VERSION;
-        }
-
-        @Override
-        public IPacketEntityAPI getInstance() {
-            return (inst == null ? (inst = new PacketEntityAPI()) : inst);
-        }
     }
 }

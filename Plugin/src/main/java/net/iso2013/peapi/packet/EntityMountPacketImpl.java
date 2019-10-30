6992f1e99aa7c5dca1d11b77b8c77d120948fc03
iso2013 <ejwmercer@gmail.com>
2019-10-29 20:17:06 -0600

    Remove erroneous length check on mount packet

--- Plugin/src/main/java/net/iso2013/peapi/packet/EntityMountPacketImpl.java
+++ Plugin/src/main/java/net/iso2013/peapi/packet/EntityMountPacketImpl.java
@@ -67,7 +67,6 @@ public class EntityMountPacketImpl extends EntityPacketImpl implements EntityMou
 
     @Override
     public PacketContainer getRawPacket() {
-        assert targets.size() > 0;
         return super.getRawPacket();
     }
 
